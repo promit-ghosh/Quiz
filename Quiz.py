99ab5753403ee4d0f039938844b00e27cf105cf0
promit-ghosh <promit.ghosh@gmail.com>
2022-06-01 22:50:10 -0400

    version1

---
+++ Quiz.py
@@ -0,0 +1,17 @@
+print("Hello Welcome to the quiz \nHere is your first question\n\n")
+
+print("Question 1 - Which is greater 49 or 94 ?")
+
+try:
+    userAns = int(input("Your answer is "))
+except ValueError:
+    print("Enter numeric input only next time")
+    print()
+except Exception as e:
+    print("Something went wrong")
+    print(e)
+else:
+    if userAns == 94:
+        print("yay !! That's correct")
+    else:
+        print("duh !! That's not correct")
